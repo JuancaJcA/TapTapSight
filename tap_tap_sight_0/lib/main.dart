e9f6877def3d1b2ff75cdf0902d2971a365a92fd
lr-salinas <laura.salinas.r@ucb.edu.bo>
2021-05-27 09:14:43 -0400

    adding icon

--- tap_tap_sight_0/lib/main.dart
+++ tap_tap_sight_0/lib/main.dart
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Tap Tap',
+      title: 'TapTapSight',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         brightness: Brightness.dark,
